d561d4f47e0341c3c6f1f81b6d4160c274d884e9
Parm <parmjitkunner@gmail.com>
2020-02-29 14:34:52 +0000

    Additional style and structure work added to site

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
+const path = require('path');
 const portNumber = 3000;
 const express = require('express');
 
 const app = express();
 
-
-
 /* Routes*/
 
 const siteRoute = require('./routes/site');
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-
 app.use(siteRoute);
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(portNumber)
--- controllers/site.js
+++ controllers/site.js
@@ -1,5 +1,13 @@
-exports.getIndex = (req, res, next ) =>{
-res.render('home/index', {
+exports.getIndex = (req, res, next) => {
+    res.render('sitePages/index', {
+        pageTitle: 'Nexus Veterinary Physiotherapy - Homepage',
+        path: '/'
+    });
+}
 
-});
+exports.getAboutPage = (res, req, next) => {
+    res.render('/about/about', {
+        pageTitle: 'About - Nexus Veterinary Physiotherapy',
+        path:'/about'
+    });
 }
